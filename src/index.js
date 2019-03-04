d34335b55943265990a689695e1ac67107cc19f3
Sapar Atayev <sopa.97@mail.ru>
2019-03-05 02:09:37 +0300

    love-trinagle/feat: add function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,34 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let arr=[];
+  for (let i=0;i<preferences.length;i++)arr.push(preferences[i]);
+  arr.sort(compareNumeric);
+  let countDif=1;
+  for(let i=0;i<arr.length;i++){
+    for(let j=i+1;j<arr.length;j++){
+      if(arr[i]!=arr[j]){
+        countDif++;
+        i=j-1;
+        break;
+      }
+    }
+  }
+  preferences.unshift(0);
+  let countTriangle=0;
+  if(countDif>=3){
+    for(let i=1; i<preferences.length; i++){
+      let a=i;
+      let b=preferences[a];
+      let c=preferences[b];
+      if(preferences[c]==a && preferences[c]!=b && preferences[c]!=c) {countTriangle++; break;}
+    }
+    return countTriangle;
+  }
+  return countTriangle;
+
+  function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a < b) return -1;
+  }
 };
